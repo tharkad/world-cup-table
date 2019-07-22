8a6de13243e4ea2485d274058b529a59d804c12f
Mike Houser <tharkad@casdra.com>
2019-07-22 11:07:32 -0700

    Updates

---
+++ precache-manifest.a40b4e02911d82fc6b5abb5a49c8f946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa03cd66c0b37c6edebcb8abc25de31b",
+    "url": "/world-cup-table/index.html"
+  },
+  {
+    "revision": "8f582371cf93d524b236",
+    "url": "/world-cup-table/static/css/main.834c671b.chunk.css"
+  },
+  {
+    "revision": "7819bc22e4728400dcb0",
+    "url": "/world-cup-table/static/js/2.e923fd6d.chunk.js"
+  },
+  {
+    "revision": "8f582371cf93d524b236",
+    "url": "/world-cup-table/static/js/main.e904eece.chunk.js"
+  },
+  {
+    "revision": "ad145d48e09f8bb49fef",
+    "url": "/world-cup-table/static/js/runtime~main.2ab2220a.js"
+  }
+]);
