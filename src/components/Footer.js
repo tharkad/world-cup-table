88f747230f3224e508cbd9a1d20f1683fcf19ed7
Mike Houser <houserm@adobe.com>
2024-06-05 15:35:02 -1000

    updated deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = (props) => {
         <React.Fragment>
             <p />
             <hr />
-            <p>React based project designed and implemented by Mike Houser (tharkad at casdra dot com).</p>
+            <p>React based project designed and implemented by Mike Houser (tharkad at gmail dot com).</p>
             <p>View and contribute to the project <a href='https://github.com/tharkad/world-cup-table'>on GitHub</a>.</p>
             <p>For use with <a href='https://boardgamegeek.com/boardgame/23604/world-cup-game'>The World Cup Game</a> by Shaun Derrick.</p>
             <p>And very useful for play with the <a href='https://boardgamegeek.com/thread/1323679/complete-world-cup-game-remake-pnp-available'>Print and Play remake</a> by Krister Dahlgren.</p>
